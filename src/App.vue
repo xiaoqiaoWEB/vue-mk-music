2432cfa5333ee6221f6065d554a0cf256efaa2f3
xiaoqiaoWEB <346043591@qq.com>
2018-10-03 23:28:52 +0800

    all first

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <div class="hello">hello</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+.hello{
+  color: chartreuse;
+}
+</style>
