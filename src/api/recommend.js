33b4b2a3ffb2902fac10062e7f0aa1048d9e883c
xiaoqiaoWEB <346043591@qq.com>
2019-01-06 13:35:22 +0800

    banner

--- src/api/recommend.js
+++ src/api/recommend.js
@@ -2,6 +2,7 @@ import jsonp from 'common/js/jsonp'
 import {commonParams, options} from './config'
 import axios from 'axios'
 
+// banner
 export function getRecommend() {
     const url = 'https://c.y.qq.com/musichall/fcgi-bin/fcg_yqqhomepagerecommend.fcg'
   
--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,151 +1,99 @@
 <template>
-    <div class="recommend">
-        <scroll class="recommend-content" ref="scroll" :data="discList">
-            <div>
-                <div class="slider-wrapper" v-if="recommends.length">
-                    <div class="slider-content">
-                        <silder ref="silder">
-                            <div v-for="(item,index) in recommends" :key="index">
-                                <a :href="item.linkUrl">
-                                    <img :src="item.picUrl" class="needclick" />
-                                </a>
-                            </div>
-                        </silder>
-                    </div>
-                </div>
-                <div class="recommend-list">
-                    <h1 class="list-title">热门歌单推荐</h1>
-                    <ul>
-                        <li class="item" v-for="(item,index) in discList" :key="index">
-                            <div class="icon">
-                                <img v-lazy="item.imgurl" :alt="item.creator.name" width="60" height="60" @load="imgLoad">
-                            </div>
-                            <div class="text">
-                                <h2 class="name">{{item.creator.name}}</h2>
-                                <p class="desc">{{item.dissname}}</p>
-                            </div>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-            <div class="loading-container" v-show="!discList.length">
-                <loading></loading>
-            </div>
-        </scroll>
-    </div>
+  <div class="recommend">
+    <scroll class="recommend-content" ref="scroll" :data="discList">
+      <div>
+        <div class="slider-wrapper" v-if="recommends.length">
+          <div class="slider-content">
+            <silder ref="silder">
+              <div v-for="(item,index) in recommends" :key="index">
+                <a :href="item.linkUrl">
+                  <img :src="item.picUrl" class="needclick">
+                </a>
+              </div>
+            </silder>
+          </div>
+        </div>
+        <div class="recommend-list">
+          <h1 class="list-title">热门歌单推荐</h1>
+          <ul>
+            <li class="item" v-for="(item,index) in discList" :key="index">
+              <div class="icon">
+                <img
+                  v-lazy="item.imgurl"
+                  :alt="item.creator.name"
+                  width="60"
+                  height="60"
+                  @load="imgLoad"
+                >
+              </div>
+              <div class="text">
+                <h2 class="name">{{item.creator.name}}</h2>
+                <p class="desc">{{item.dissname}}</p>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="loading-container" v-show="!discList.length">
+        <loading></loading>
+      </div>
+    </scroll>
+  </div>
 </template>
 
 <script type="text/ecmascript-6">
-    import Scroll from 'base/scroll/scroll'
-    import Silder from 'base/silder/silder'
-    import Loading from 'base/loading/loading'
+import Scroll from 'base/scroll/scroll'
+import Silder from 'base/silder/silder'
+import Loading from 'base/loading/loading'
 
-    import { getRecommend, getDiscList } from 'api/recommend'
-    import { ERR_OK } from 'api/config'
+import { getRecommend, getDiscList } from 'api/recommend'
+import { ERR_OK } from 'api/config'
 
-    export default {
-        data () {
-            return {
-               recommends: [],
-               discList: []
-            }
-        },
-        created() {
-            this._getRecommend()
-            setTimeout(() => {
-                this._getDiscList()
-            }, 1000)
-        },
-        methods: {
-            _getRecommend() {
-                getRecommend().then((res) => {
-                    if (res.code === ERR_OK) {
-                        this.recommends = res.data.slider
-                    }
-                })
-            },
-            _getDiscList() {
-                getDiscList().then((res) => {
-                    if (res.code === ERR_OK) {
-                        this.discList = res.data.list
-                    }
-                })
-            },
-            imgLoad() {
-                if (!this.imgChecked) {
-                    this.imgChecked = true
-                    // 图片加载完成 重新计算 scroll 高度
-                    this.$refs.scroll.refresh()
-                }
-            }
-        },
-        components: {
-            Scroll,
-            Silder,
-            Loading
-        }
+export default {
+  data() {
+    return {
+      recommends: [],
+      discList: []
     }
+  },
+  created() {
+    this._getRecommend()
+    setTimeout(() => {
+      this._getDiscList()
+    }, 1000)
+  },
+  methods: {
+    _getRecommend() {
+      getRecommend().then(res => {
+        if (res.code === ERR_OK) {
+          this.recommends = res.data.slider
+        }
+      })
+    },
+    _getDiscList() {
+      getDiscList().then(res => {
+        if (res.code === ERR_OK) {
+          this.discList = res.data.list
+        }
+      })
+    },
+    imgLoad() {
+      if (!this.imgChecked) {
+        this.imgChecked = true
+        // 图片加载完成 重新计算 scroll 高度
+        this.$refs.scroll.refresh()
+      }
+    }
+  },
+  components: {
+    Scroll,
+    Silder,
+    Loading
+  }
+}
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
-   @import "~common/stylus/variable"
-
-  .recommend
-    position: fixed
-    width: 100%
-    top: 88px
-    bottom: 0
-    .recommend-content
-      height: 100%
-      overflow: hidden
-      .slider-wrapper
-        position: relative
-        width: 100%
-        height: 0
-        padding-top: 40%
-        overflow: hidden
-        .slider-content
-          position: absolute
-          top: 0
-          left: 0
-          width: 100%
-          height: 100%
-      .recommend-list
-        .list-title
-          height: 65px
-          line-height: 65px
-          text-align: center
-          font-size: $font-size-medium
-          color: $color-theme
-        .item
-          display: flex
-          box-sizing: border-box
-          // Y轴方向才居中对齐 
-          align-items: center
-          padding: 0 20px 20px 20px
-          .icon
-            flex: 0 0 60px
-            width: 60px
-            padding-right: 20px
-          .text
-            display: flex
-            // 设置 Y轴 为主轴
-            flex-direction: column
-            // 元素在主轴中间,富裕空间在主轴两侧
-            justify-content: center
-            flex: 1
-            line-height: 20px
-            overflow: hidden
-            font-size: $font-size-medium
-            .name
-              margin-bottom: 10px
-              color: $color-text
-            .desc
-              color: $color-text-d
-        .loading-container
-            position: absolute
-            width: 100%
-            top: 50%
-            transform: translateY(-50%)
+@import './recommend'
 </style>
 
