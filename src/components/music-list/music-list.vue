a0a9dcffa3ad1a67afe99000d15b65247714f69f
xiaoqiaoWEB <346043591@qq.com>
2019-01-15 23:04:53 +0800

    music-list

--- src/components/music-list/music-list.vue
+++ src/components/music-list/music-list.vue
@@ -1,13 +1,20 @@
 <template>
   <div class="music-list">
-    <div class="back">
+    <div class="back" @click="back">
       <i class="icon-back"></i>
     </div>
     <h1 class="title" v-html="title"></h1>
     <div class="bg-imgage" :style="bgstyle" ref="bgImage">
-      <div class="filter"></div>
+      <div class="play-wrapper">
+        <div class="play" ref="play">
+          <i class="icon-play"></i>
+          <span class="text">随机播放全部</span>
+        </div>
+      </div>
+      <div class="filter" ref="filter"></div>
     </div>
-    <scroll :data="songs" class="list" ref="list">
+    <div class="bg-layer" ref="layer"></div>
+    <scroll @scroll="scroll" :probeType="probeType" :listenScroll="listenScroll" :data="songs" class="list" ref="list">
       <div class="song-list-wrapper">
         <song-list :songs="songs"></song-list>
       </div>
@@ -17,6 +24,12 @@
 <script type="text/ecmascript-6">
 import Scroll from 'base/scroll/scroll'
 import SongList from 'base/song-list/song-list' 
+import {prefixStyle} from 'common/js/dom'
+
+const RESERVED_HEIGHT = 40
+const transform = prefixStyle('transform')
+const backdrop = prefixStyle('backdrop-filter')
+
 export default {
   props: {
     bgImage: {
@@ -32,13 +45,66 @@ export default {
       default: ''
     }
   },
+  data() {
+    return {
+      scrollY: 0
+    }
+  },
   computed: {
     bgstyle() {
       return `background-image:url(${this.bgImage})`
     }
   },
+  created() {
+    this.probeType = 3
+    this.listenScroll = true
+  },
   mounted() {
-    this.$refs.list.$el.style.top = this.$refs.bgImage.clientHeight + 'px'
+    this.imageHeight = this.$refs.bgImage.clientHeight
+    this.minTransalteY = -this.imageHeight + RESERVED_HEIGHT
+    this.$refs.list.$el.style.top = this.imageHeight + 'px'
+  },
+  watch: {
+    scrollY(newY) {
+      let transalteY = Math.max(this.minTransalteY, newY)
+      let zIndex = 0
+      let scale = 1
+      let blur = 0
+
+      let percent = Math.abs(newY / this.imageHeight)
+      if (newY > 0) {
+        scale = 1 + percent
+        zIndex = 10
+      } else {
+        blur = Math.min(percent * 20, 20)
+      }
+
+      this.$refs.layer.style[transform] = `translate3d(0,${transalteY}px,0)`
+
+      // iOS高斯模糊
+      this.$refs.filter.style[backdrop] = `blur(${blur})`
+
+      if (newY < transalteY) {
+        zIndex = 10
+        this.$refs.bgImage.style.paddingTop = 0
+        this.$refs.bgImage.style.height = `${RESERVED_HEIGHT}px`
+        this.$refs.play.style.display = 'none'
+      } else {
+        this.$refs.bgImage.style.paddingTop = '70%'
+        this.$refs.bgImage.style.height = 0
+        this.$refs.play.style.display = ''
+      }
+      this.$refs.bgImage.style.zIndex = zIndex
+      this.$refs.bgImage.style[transform] = `scale(${scale})`
+    }
+  },
+  methods: {
+    scroll(pos) {
+      this.scrollY = pos.y
+    },
+    back() {
+      this.$router.back()
+    }
   },
   components: {
     Scroll,
